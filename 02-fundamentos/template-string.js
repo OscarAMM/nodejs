756d04871d966b16d59f8e79e6d999a6e47c3576
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-10-13 12:50:32 -0500

    Template string vs normal concat

---
+++ 02-fundamentos/template-string.js
@@ -0,0 +1,8 @@
+const nombre = "Deadpool";
+const real = "Wade Winston";
+
+//string concatenate
+const normal = nombre + ' ' +real;
+//template string
+const templateString = `${nombre} ${real}`;
+console.log(normal);
