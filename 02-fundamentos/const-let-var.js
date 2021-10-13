35aa021dbc83f3dfe347bf54b5943f361ac69a8c
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-10-13 12:43:19 -0500

    Const-var-let exercise

---
+++ 02-fundamentos/const-let-var.js
@@ -0,0 +1,14 @@
+/**
+ * Var should not be used ever
+ * const is for variables that keeps the same value
+ * let will be used when you know the data will change at some point
+ * CTM Alana
+ */
+const nombre = "Puerco arana";
+
+if(true){
+    const nombre = "Burrito Sabanero";
+    console.log(nombre);
+}
+
+console.log(nombre);
