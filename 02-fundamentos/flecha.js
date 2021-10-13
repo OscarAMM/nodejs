48028d4e6ada71d39168b29dacf64082646e8dde
Oscar Monsreal <A15216352@alumnos.uady.mx>
2021-10-13 14:58:28 -0500

    Arrow functions

---
+++ 02-fundamentos/flecha.js
@@ -0,0 +1,13 @@
+//normal function sintaxis
+function sumar(a,b){
+    return a+b;
+}
+console.log(sumar(10,23));
+//arrow functions
+const sumarFlecha = (a,b) =>{
+    return a + b;
+}
+const greetings = () => "Hola, puercos";
+
+console.log(greetings());
+console.log(sumarFlecha(12,43));
