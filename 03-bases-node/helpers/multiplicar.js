cd3fa14190b2fc2cce26bf843dc9116345592c91
Oscar Monsreal <A15216352@alumnos.uady.mx>
2022-02-08 19:00:03 -0600

    [Update] added h parameter

--- 03-bases-node/helpers/multiplicar.js
+++ 03-bases-node/helpers/multiplicar.js
@@ -1,12 +1,15 @@
 const fs = require('fs');
 
-const crearArchivo = async (base = 5) => {
+const crearArchivo = async (base = 5, listar = false, hasta = 10)  => {
     try {
         let salida = ' ';
-        for (let i = 1; i < 11; i++) {
+        for (let i = 1; i <= `${hasta}`; i++) {
             salida += `${base} X ${i} = ${base * i}\n`;
         }
-        fs.writeFileSync(`tabla-${base}.txt`, salida);
+        fs.writeFileSync(`./salida/tabla-${base}.txt`, salida);
+        if (listar == true) {
+            console.log(salida);
+        }
         return `tabla-${base}.txt`
 
     } catch (error) {
