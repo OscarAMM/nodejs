59d2da14efd664ca37e14359142eee8011b7ab62
Oscar Monsreal <A15216352@alumnos.uady.mx>
2022-02-08 18:59:18 -0600

    [Update] Added argv h to be displayed

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,10 +1,13 @@
 //print multiplicity of 5
-
 const { crearArchivo } = require('./helpers/multiplicar');
+const argv = require('./config/yargs');
+
 console.clear();
+console.log(argv);
 const base = 9;
 
-crearArchivo(base)
+
+crearArchivo(argv.b, argv.l, argv.h)
     .then(nombreArchivo => {
         console.log(nombreArchivo, 'creado');
     })
